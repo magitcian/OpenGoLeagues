8da462229932e4ba0915a739a9b24e6818086645
Séverine Schiltz <schiltz.severine@gmail.com>
2022-05-19 22:09:40 +0200

    fix : api url in frontend

--- frontend/src/helpers/URLContext.js
+++ frontend/src/helpers/URLContext.js
@@ -3,6 +3,6 @@
 //Prod:
 //export const url = 'https://opengoleagues.magitcian.com/api/';
 // //Dev
-//export const url = 'http://localhost:3001/';
-export const url = 'http://192.168.1.20:3001/';
+export const url = 'http://localhost:3001/';
+//export const url = 'http://192.168.1.20:3001/';
 
